494bb34ee020c0b66f440151988da423d83f8061
Arka Mandal <arkamandal764@gmail.com>
2024-04-30 19:45:12 +0530

    hi

--- indranil friend/heart.py
+++ indranil friend/heart.py
@@ -33,14 +33,14 @@ plt.title('Target Value Distribution')
 plt.show()
 
 # DAta analysis between sex and target i.e chance of heart disease
-print(heart['sex'].unique())
+#print(heart['sex'].unique())
 
-sns.set_style("whitegrid")
+#sns.set_style("whitegrid")
 
-plt.figure(figsize=(8,6))
-sns.barplot(x='sex', y='target',data=heart, palette='pastel')
+#plt.figure(figsize=(8,6))
+'''sns.barplot(x='sex', y='target',data=heart, palette='pastel')
 plt.title('Relation b/w Sex and target')
-plt.show()
+#plt.show()
 
 # Data analysis between chest pain(cp) and target i.e chance of heart disease
 print(heart['cp'].unique())
@@ -50,7 +50,7 @@ sns.set_style('whitegrid')
 plt.figure(figsize=(8,6))
 sns.barplot(x='cp', y='target', data=heart, palette='pastel')
 plt.title('Relation b/w Chest Pain and Target')
-plt.show()
+#plt.show()
 # Data analysis between cholesterol and target i.e chance of heart disease
 print(heart['chol'].unique())
 
@@ -59,7 +59,7 @@ sns.set_style('whitegrid')
 plt.figure(figsize=(8,6))
 sns.barplot(x='chol', y='target', data=heart, palette='pastel')
 plt.title('Relation b/w Cholesterol and Target')
-plt.show()
+#plt.show()
 
 # Data analysis between fasting blood pressure(fbs) and target i.e chance of heart disease
 print(heart['fbs'].unique())
@@ -67,10 +67,51 @@ print(heart['fbs'].unique())
 sns.set_style('whitegrid')
 
 plt.figure(figsize=(8,6))
-sns.barplot(x='fbs', y='target', data=heart, palette='pastel')
+sns.kdeplot(x='fbs', y='target', data=heart, palette='pastel')
 plt.title('Relation b/w Fasting blood pressure and Target')
-plt.show()
+#plt.show()'''
 
+#plotting all 4 graphs in one plot
+#1 : sex vs target
+plt.subplot(2,2,1)
+print(heart['sex'].unique())
+
+sns.set_style("whitegrid")
+
+
+sns.barplot(x='sex', y='target',data=heart, palette='pastel')
+plt.title('Relation b/w Sex and target')
+
+
+#2 chest pain vs target
+plt.subplot(2,2,2)
+print(heart['cp'].unique())
+
+sns.set_style('whitegrid')
+
+sns.barplot(x='cp', y='target', data=heart, palette='pastel')
+plt.title('Relation b/w Chest Pain and Target')
+
+
+#3: cholesterol vs target
+plt.subplot(2,2,3)
+print(heart['chol'].unique())
+
+sns.set_style('whitegrid')
+
+sns.scatterplot(x='target', y='chol', data=heart, palette='pastel')
+plt.title('Relation b/w Cholesterol and Target')
+
+#4 resting blood pressure vs target
+plt.subplot(2,2,4)
+print(heart['trestbps'].unique())
+
+sns.set_style('whitegrid')
+
+sns.scatterplot(x='target', y='trestbps', data=heart, palette='pastel')
+plt.title('Relation b/w Resting blood pressure and Target')
+
+plt.show()
 #get correlations of each features in heartset
 corrmat = heart.corr()
 top_corr_features = corrmat.index
